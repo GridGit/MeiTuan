414a8e431cc558e6c63149f4e8d695bd10a314b5
GridGit <gridshuai@gmail.com>
2017-06-25 12:32:07 +0800

    init dev env by yo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meiTuanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the meiTuanApp
+ */
+angular.module('meiTuanApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
